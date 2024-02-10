37f0f0b0728be71f03ad18fc435337ac7933fb60
HASSAN METWALY <131253773+HassanMetwaly@users.noreply.github.com>
2024-02-10 14:27:45 +0200

    Update 2024_02_09_123440_create_tasks_table.php

--- database/migrations/2024_02_09_123440_create_tasks_table.php
+++ database/migrations/2024_02_09_123440_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('dscription')->nullable();
-            $table->text('isDone')->default(false);
+            $table->boolean('isDone')->default(false);
             $table->timestamps();
         });
     }
